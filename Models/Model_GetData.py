dd854368ecdf7ad1c4b47b8c21dce13b943b028f
ManivelSethuEVA5 <67995700+ManivelSethuEVA5@users.noreply.github.com>
2020-09-19 20:26:23 +0530

    Create Model_GetData.py

---
+++ Models/Model_GetData.py
@@ -0,0 +1,35 @@
+# Code for downloading the data
+from torchvision import datasets
+from torchvision import transforms
+
+
+from shrinkai.data_process.transformations import *
+
+
+# from transformations import *
+
+transformations = GetTransforms()
+train_transforms = transforms.Compose(transformations.trainparams())
+test_transforms = transforms.Compose(transformations.testparams())
+
+print(train_transforms)
+
+class Get_MNISTTrainData():
+    def __init__(self, dir_name:str):
+        self.dirname = dir_name
+
+    def download_train_data(self):
+        return datasets.MNIST('./data', train=True, download=True, transform=train_transforms)
+
+    def download_test_data(self):
+        return datasets.MNIST('./data', train=False, download=True, transform=test_transforms)
+
+class GetCIFAR10_TrainData():
+    def __init__(self, dir_name:str):
+        self.dirname = dir_name
+
+    def download_train_data(self):
+        return datasets.CIFAR10('./data', train=True, download=True, transform=train_transforms)
+
+    def download_test_data(self):
+        return datasets.CIFAR10('./data', train=False, download=True, transform=test_transforms)
